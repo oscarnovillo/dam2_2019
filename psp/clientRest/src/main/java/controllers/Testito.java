074a17f6772cba103ccd66a0de0795115cdd2776
oscar <oscar@2DAM-00.info.quevedo.local>
2019-10-21 09:26:43 +0200

    SDFSDF

---
+++ psp/clientRest/src/main/java/controllers/Testito.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Testito implements Initializable {
+
+
+    @FXML
+    private Button cito;
+
+    @FXML
+    private void clickButton(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
