0c066bb7938f5f1a4b1a7815b8b325d87328c581
oscar.novillo <oscar.novillo@gmail.com>
2019-11-05 20:05:46 +0100

    hibernate

--- accesoDatos/hibernateTest/src/main/java/modelo/TeacherEntityjpa.java
+++ accesoDatos/hibernateTest/src/main/java/modelo/TeacherEntityjpa.java
@@ -5,7 +5,7 @@ import java.sql.Date;
 import java.util.Collection;
 
 @Entity
-@Table(name = "teacher", schema = "test", catalog = "")
+@Table(name = "teacher")
 public class TeacherEntityjpa {
   private int idteacher;
   private String type;
