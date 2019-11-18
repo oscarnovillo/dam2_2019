32c3bd696dc02e964ec7e0059f25229d59540835
oscar <oscar@2DAM-00.info.quevedo.local>
2019-11-18 10:15:52 +0100

    ertert

--- psp/okhttpRetrofit/src/main/java/retrofit/LoginApi.java
+++ psp/okhttpRetrofit/src/main/java/retrofit/LoginApi.java
@@ -2,9 +2,7 @@ package retrofit;
 
 import modelo.Usuario;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
+import retrofit2.http.*;
 
 import java.util.List;
 
@@ -13,6 +11,17 @@ public interface LoginApi {
   @POST("login")
   Call<String> login(@Body Usuario usuario);
 
+  @PUT("login")
+  Call<Usuario> actualizar(@Body Usuario usuario);
+
+  @DELETE("login")
+  Call<String> borrar(@Query("id") int idUsuario);
+
   @GET("login")
   Call<List<Usuario>> login ();
+
+  @GET("login")
+  Call<Usuario> login (@Query("id") int idUsuario);
+
+
 }
--- psp/payara/src/main/java/servlets/Usuario.java
+++ psp/payara/src/main/java/servlets/Usuario.java
@@ -7,12 +7,22 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.stream.Collectors;
 
 @WebServlet(name = "Usuario",urlPatterns = {"/login"})
 public class Usuario extends javax.servlet.http.HttpServlet {
+
+  @Override
+  protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    Jsonb jsonb = JsonbBuilder.create();
+    dao.modelo.Usuario user = jsonb.fromJson(req.getReader(), dao.modelo.Usuario.class);
+
+  }
 
   protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
@@ -28,8 +38,17 @@ public class Usuario extends javax.servlet.http.HttpServlet {
   private ServiciosUsuarios su;
 
   protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-    Jsonb jsonb = JsonbBuilder.create();
-    jsonb.toJson(su.getUsers(),response.getWriter());
+    if (request.getParameter("id") != null)
+    {
+
+    }
+    else
+    {
+      Jsonb jsonb = JsonbBuilder.create();
+      jsonb.toJson(su.getUsers(),response.getWriter());
+//      response.setStatus(403);
+//      response.getWriter().println("mensaje del error");
+    }
   }
 
 
