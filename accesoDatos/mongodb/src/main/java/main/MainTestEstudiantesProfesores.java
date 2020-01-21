e4328d993cc8fafe8d57d9e2f2c319768fd6fa2b
oscar.novillo <oscar.novillo@gmail.com>
2020-01-21 23:24:26 +0100

    mongodb

--- accesoDatos/mongodb/src/main/java/main/MainTestEstudiantesProfesores.java
+++ accesoDatos/mongodb/src/main/java/main/MainTestEstudiantesProfesores.java
@@ -5,6 +5,7 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.UpdateOptions;
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
@@ -33,11 +34,19 @@ public class MainTestEstudiantesProfesores {
 
     col.distinct("asignaturas.nombre",String.class).into(new ArrayList<>()).forEach(System.out::println);
 
-    //col.updateOne(Document.parse("{\"nif\":\"profe\"}"),Document.parse("{\"$push\":{\"asignaturas\":{\"nombre\":\"testing\"}}}"));
+    col.updateOne(Document.parse("{\"nif\":\"profe\"}"),Document.parse("{\"$push\":{\"asignaturas\":{\"nombre\":\"testing\"}}}"));
+    col.updateOne(Document.parse("{\"nif\":\"profe\"}"),Document.parse("{\"$push\":{\"asignaturas\":{\"nombre\":\"testing\"}}}"));
+    col.updateOne(Document.parse("{\"nif\":\"profeTest\"}"),Document.parse("{\"$push\":{\"asignaturas\":{\"nombre\":\"testing\"}}}"));
+
 
     col.distinct("asignaturas.nombre",String.class).into(new ArrayList<>()).forEach(System.out::println);
 
-    col.updateOne(Document.parse("{\"asignaturas.nombre\":\"testing\"}"),Document.parse("{\"$set\":{\"asignaturas.$$.nombre\":\"cambio\"}}"));
+    //cambia la primera ocurrencia por documento
+    //col.updateMany(Document.parse("{\"asignaturas.nombre\":\"testing\"}"),Document.parse("{\"$set\":{\"asignaturas.$.nombre\":\"cc\"}}"));
+
+    //cambia todo
+   col.updateMany(Document.parse("{\"asignaturas.nombre\":\"cambio\"}"),Document.parse("{\"$set\":{\"asignaturas.$[asig].nombre\":\"22\"}}"),
+       new UpdateOptions().arrayFilters(List.of(Document.parse("{\"asig.nombre\" : { $eq: \"cambio\" } }"))));
 
     col.distinct("asignaturas.nombre",String.class).into(new ArrayList<>()).forEach(System.out::println);
 
@@ -50,10 +59,18 @@ public class MainTestEstudiantesProfesores {
     col.aggregate(List.of(Document.parse("{$unwind : \"$asignaturas\"}"),Document.parse("{$set : {maxConv : {$max:\"$asignaturas.notas.convocatoria\"}}}"),
         Document.parse("{$unwind: \"$asignaturas.notas\"}"),
             Document.parse("{$match : { $expr: { $eq: [ \"$asignaturas.notas.convocatoria\" , \"$maxConv\" ] } }}"),
-            Document.parse("{$set : {nota: \"$asignaturas.notas.nota\",asignatura:\"$asignaturas.nombre\"}}"),
-            Document.parse("{$project : {\"nota\":1,\"nombre\":1,\"asignatura\":1,\"_id\":0}}")
+//            Document.parse("{$set : {nota: \"$asignaturas.notas.nota\",asignatura:\"$asignaturas.nombre\"}}"),
+            Document.parse("{$project : {\"nota\":\"$asignaturas.notas.nota\",\"nombre\":1,\"asignatura\":\"$asignaturas.nombre\",\"_id\":0}}")
             ))
-        .map(document -> document.getString("nombre")+" tiene "+document.getInteger("nota")).into(new ArrayList<>()).forEach(System.out::println);
+        .map(document -> document.getString("nombre")+" tiene "+document.getInteger("nota")+ " en "+document.getString("asignatura")).into(new ArrayList<>()).forEach(System.out::println);
+
+    col.aggregate(List.of(Document.parse("{$match: {\"asignaturas.nombre\" : { $in : [\"Lengua\",\"Quimica\"]}}}"),
+        Document.parse("{$unwind : \"$asignaturas\"}"),
+        Document.parse("{$group: {_id : \"$asignaturas.nombre\",numeroAlumnos : { $sum: 1} }}"),
+        Document.parse("{$project: { nombre: \"$_id\",numeroAlumnos:1,_id:0}}")
+    ))
+        .map(document -> document.getString("nombre")+" tiene "+document.getInteger("numeroAlumnos")+ " alumnos ").into(new ArrayList<>()).forEach(System.out::println);
+
 
 
 /*
@@ -69,6 +86,12 @@ public class MainTestEstudiantesProfesores {
 {$set : {nota: "$asignaturas.notas.nota",nombre:"$asignaturas.nombre"}},
 {$project : {"nota":1,"nombre":1,"_id":0}}
 
+
+// numero alumnos por asignatura
+{$match: {"asignaturas.nombre" : { $in : ["Lengua","Quimica"]}}},
+{$unwind : "$asignaturas"},
+{$group: {_id : "$asignaturas.nombre",numeroAlumnos : { $sum: 1} }},
+{$project: { nombre: "$_id",numeroAlumnos:1,_id:0}}
 
  */
   }
